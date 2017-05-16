6e0f6e7bb66b401eb66d77045f282fcc626f91b9
Tero Tuomala <tero.tuomala@helsinki.fi>
2017-05-16 19:14:45 +0300

    Added new component

---
+++ src/components/Timetable.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Grid} from 'react-bootstrap';
+
+class Timetable extends Component {
+  render() {
+    return (
+      <Grid>
+        
+      </Grid>
+    )
+  }
+}
