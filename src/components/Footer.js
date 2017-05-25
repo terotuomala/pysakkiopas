4ecb7d3ea6f209bd48a74ea6d568a989c0ee5631
Tero Tuomala <tero.tuomala@helsinki.fi>
2017-05-25 20:40:19 +0300

    Changed footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import {Grid} from 'react-bootstrap';
+import './Footer.css';
 
 class Footer extends Component {
   render() {
     return (
-      <Grid>
-        <hr/>
-        <footer>
-          <p>© Tero Tuomala 2017</p>
-        </footer>
-      </Grid>
+      <div>
+      <footer className="footer text-center">
+        <p>Made with <i className="fa fa-heart-o" aria-hidden="true"></i> by Tero Tuomala</p>
+      </footer>
+    </div>
     )
   }
 }
