c1802a5132f4256e2062cbe5bb64dda9e0583243
Tero Tuomala <tero.tuomala@helsinki.fi>
2017-05-26 20:56:25 +0300

    Added some content and styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {Col, Grid} from 'react-bootstrap';
+import './About.css';
 
 class About extends Component {
   render() {
     return (
       <Grid>
-        <Col lg={8} lgOffset={2} md={10} mdOffset={1}>
-        <h3>Tietoja pysäkkioppaasta</h3>
-      </Col>
+        <Col lg={8} lgOffset={2} md={2} mdOffset={1} className="text-center">
+          <a href="https://github.com/terotuomala/pysakkiopas" target="blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+        </Col>
       </Grid>
     )
   }
